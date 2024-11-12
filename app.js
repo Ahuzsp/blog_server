02c79897f1c81e298249d0622fa9a1730a02f15f
snow丶mm <2391910724@qq.com>
2024-11-12 22:57:12 +0800

    fix.上传接口

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const connectDB = require('./src/config/db.js')
-const userRoutes = require('./src/routes/userRoutes.js')
-const articleRoutes = require('./src/routes/articleRoutes.js')
-const logRoutes = require('./src/routes/logRoutes.js')
-const uploadRoutes = require('./src/routes/uploadRoutes.js')
-require('dotenv').config()
+const express = require("express")
+const cors = require("cors")
+const connectDB = require("./src/config/db.js")
+const userRoutes = require("./src/routes/userRoutes.js")
+const articleRoutes = require("./src/routes/articleRoutes.js")
+const logRoutes = require("./src/routes/logRoutes.js")
+const uploadRoutes = require("./src/routes/uploadRoutes.js")
+require("dotenv").config()
 
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require("multer")
+const upload = multer({ dest: "/tmp/uploads/" })
 
 const app = express()
 // 使用 cors 中间件
@@ -20,12 +20,12 @@ connectDB()
 app.use(express.json())
 
 // Routes
-app.use('/api/users', userRoutes)
-app.use('/api/articles', articleRoutes)
-app.use('/api/logs', logRoutes)
-app.use('/upload', upload.single('file'), uploadRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/articles", articleRoutes)
+app.use("/api/logs", logRoutes)
+app.use("/upload", upload.single("file"), uploadRoutes)
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`)
 })
 module.exports = app
